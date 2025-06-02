bbbb6c425b2652a59e28c5e218f01da0a0350fb4
webdeveloper0025 <ahammedishtiaknur2.0@gmail.com>
2025-06-02 09:43:50 +0600

    Add files via upload

---
+++ practice1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,s;
+    printf("Vai ekta value den=");
+    scanf("%d",&a);
+    printf("Vai arekta value den=");
+    scanf("%d",&b);
+    s=a+b;
+    printf("duita jog kore=%d",s);
+    return 0;
+}
